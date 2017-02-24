0ad9f2349dd0a393b9102bf977cce6406c28b43b
root <root@localhost.localdomain>
2017-02-24 13:03:03 -0500

    Client Asyncore update, Server updated

--- server.py
+++ server.py
@@ -3,29 +3,42 @@ import sys
 import json
 import asyncore
 import time
+import collections
 
 HOST = 'localhost'
 PORT = 5555
 
 class ServerHandler(asyncore.dispatcher_with_send):
+
+    def __init__(self, socket, server):
+        self.sock = socket
+        self.server = server
+
     def handle_read(self):
-        data = self.recv(1024)
+        self.data = self.recv(1024)
+        self.server.broadcast(self.data)
+
+    def parsemsg(self, data): 
         if data:
             udata = json.loads(data)
+            
             umsg = udata["message"]
             uname = udata["uname"]
-            
-
             localtime = time.localtime(time.time())
             utime = "{}:{}:{} - ".format(localtime.tm_hour, localtime.tm_min, localtime.tm_sec) 
+            
             fmsg = utime + uname + ': ' + umsg
             print 'Received message from ' + uname + ': ' + umsg
             print 'Formatted message: ' + fmsg
             if (umsg == "/quit") or (umsg == "/exit"):
                 self.close()
                 sys.exit()
-        
-            self.sendall(fmsg)
+            return fmsg
+    
+    def handle_write(self):
+        data = parsemsg(self.data)
+        self.sendall(fmsg)
+
 
 class ServerSocket(asyncore.dispatcher):
     def __init__(self, host, port):
@@ -37,15 +50,21 @@ class ServerSocket(asyncore.dispatcher):
         print 'Socket bound'
         self.listen(5)
         print 'Socket listening on port ', PORT
+        self.clients = []
+
     def handle_accept(self):
         pair = self.accept()
         if pair is not None:
             sock, address = pair
             print "Connected from ", address
-            ServerHandler(sock)
+            self.clients.append(ServerHandler(sock, self))
 
     def handle_close(self):
         self.close()
+
+    def broadcast(self, msg):
+        for client in self.clients:
+            client.handle_write(msg)
 
 server = ServerSocket(HOST, PORT)
 asyncore.loop()
